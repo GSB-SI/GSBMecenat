e682ed12ae04a899d9677054dbafd19838a8bcdb
BarckN <nolanbarck@gmail.com>
2024-11-18 11:34:21 +0100

    ADD scrypts sql

--- script Sql/procédures-stockées-BDDProGestion.sql
+++ script Sql/procédures-stockées-BDDProGestion.sql
@@ -24,7 +24,7 @@ go
 create  procedure sp_country_add ( @name varchar(40), @isoCode varchar(3))
 as
 begin
-	insert into country values(null,@name,@isoCode)
+	insert into country(name,isoCode) values(@name,@isoCode)
 end
 go
 
@@ -61,7 +61,7 @@ go
 create procedure sp_actions_add( @libel varchar(40))
 as
 begin
-	insert into actions values(null,@libel)
+	insert into actions(libel) values(@libel)
 end
 go
 
@@ -98,7 +98,7 @@ go
 create procedure sp_users_add( @login varchar(40),@password varchar(40),@role_id int)
 as
 begin
-	insert into Users values (null, @login,@password,@role_id)
+	insert into Users(login,password,role_id) values (@login,@password,@role_id)
 end
 go
 
@@ -132,19 +132,20 @@ begin
 end
 go
 
-create procedure sp_associations_add( @name varchar(40), @mission_id int, @country_id int)
+create procedure sp_associations_add( @name varchar(40),@responsable varchar(40), @mission_id int, @country_id int)
 as
 begin
-	insert into associations values(null, @name, @mission_id, @country_id)
+	insert into associations(name,responsable,mission_id,country_id) values(@name, @responsable, @mission_id, @country_id)
 end
 go
 
-create procedure sp_associations_update(@id int, @name varchar(40), @mission_id int, @country_id int)
+create procedure sp_associations_update(@id int, @name varchar(40), @responsable varchar(40), @mission_id int, @country_id int)
 as
 begin
-	update associations set name=@name, mission_id=@mission_id, country_id=@country_id where id=@id
+	update associations set name=@name,responsable=@responsable, mission_id=@mission_id, country_id=@country_id where id=@id
 end
 go
+
 
 create procedure sp_associations_delete(@id int)
 as
@@ -161,7 +162,7 @@ begin
 end
 go
 
-create procedure sp_associations_getByYear (@choosenYear int)
+create procedure sp_choosenCountryYears_getByYear (@choosenYear int)
 as
 begin
 	Select * from ChoosenCountryYears where choosenYear=@choosenYear
@@ -175,10 +176,10 @@ begin
 end
 go
 
-create procedure sp_choosenCountryYears_update(@country_id int, @choosenYear int)
+create procedure sp_choosenCountryYears_update(@country_id int,  @oldChoosenYear int, @newChoosenYear int)
 as
 begin
-	update ChoosenCountryYears set choosenYear=@choosenYear where country_id=@country_id and choosenYear=@choosenYear
+	update ChoosenCountryYears set choosenYear=@newChoosenYear where country_id=@country_id and choosenYear=@oldChoosenYear
 end
 go
 
@@ -229,7 +230,7 @@ go
 create procedure sp_interventionLogs_add(@tableName varchar(40), @operation varchar(6), @reference int)
 as
 begin
-	Insert into InterventionsLogs values (null, @tableName, @operation, @reference)
+	Insert into InterventionsLogs(tableName,operation,reference) values (@tableName, @operation, @reference)
 end
 go
 
@@ -252,7 +253,7 @@ go
 create procedure sp_missions_add(@libel varchar(40))
 as
 begin
-	Insert into Missions values (null, @libel)
+	Insert into Missions(libel) values (@libel)
 end
 go
 
@@ -310,7 +311,7 @@ go
 create procedure sp_partnerships_add(@action_id int, @association_id int, @country_id int)
 as
 begin
-	insert into Partnerships(id, date, action_id, association_id, country_id) values(null,getdate(), @action_id, @association_id, @country_id)
+	insert into Partnerships(date, action_id, association_id, country_id) values(getdate(), @action_id, @association_id, @country_id)
 end
 go
 
@@ -367,7 +368,7 @@ go
 create procedure sp_role_add(@libel varchar(40))
 as
 begin
-	insert into Role values(null, @libel)
+	insert into Role(libel) values(@libel)
 end
 go
 

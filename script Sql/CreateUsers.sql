e15034ff9834212ec4d6b1336029bb0c283d9fef
BarckN <nolanbarck@gmail.com>
2024-12-02 11:38:24 +0100

    feats(script): add CreateUsers.sql and update Role.sql

---
+++ script Sql/CreateUsers.sql
@@ -0,0 +1,4 @@
+insert into Users(login,password,role_id) values('admin','$2a$13$2FttJborJs4im1apbOHHPOQxtsnNsS2OSzt1GhslOWiIalv98iHf.',1)
+insert into Users(login,password,role_id) values('directFinan','$2a$13$V/qJf/pAnkIN5AKYfFU2J.FNVJgnonu4PN1g5bYxWeJHvpOp4d4oO',2)
+insert into Users(login,password,role_id) values('directPartCom','$2a$13$9rNU1AmGgUIETaFrhqWmW.zEf5BLerZjQc/R7ABfJBcYoztkOCwOi',3)
+insert into Users(login,password,role_id) values('user','$2a$13$KScWqv3KmOTrPPWHuE40leubSVu4sdploPn/XkKiZphID7PGQHf.W',4)
--- script Sql/Role.sql
+++ script Sql/Role.sql
@@ -1,3 +1,4 @@
-insert into Role(libel) values('Admin')
+insert into Role(libel) values('Administrateur')
 insert into Role(libel) values('Directeur du service Financier')
 insert into Role(libel) values('Directeur du service Partenariat et Communication')
+insert into Role(libel) values('Utilisateur')
